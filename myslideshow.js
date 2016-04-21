5e675ad2aa26401011b665dfba32a63af57d680e
gyx13 <guoyuxuan13@gmail.com>
2016-04-22 00:43:22 +0800

    script frame almost complete meh~

---
+++ myslideshow.js
@@ -0,0 +1,13 @@
+;(function($, window, document, undefined){
+	var Myslideshow = function(ele, options){
+
+	};
+
+	Myslideshow.prototype = {
+
+	};
+	
+	$.fn.myslideshow = function(options){
+		var mySlideShow = new Myslideshow(this,options);
+	};
+})(jQuery, window, document);
