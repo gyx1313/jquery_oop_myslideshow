5bb98405324679c2a44c1a49ae7dc317aa22b46e
gyx13 <guoyuxuan13@gmail.com>
2016-04-25 01:20:38 +0800

    2016.04.24

--- myslideshow.js
+++ myslideshow.js
@@ -1,13 +1,21 @@
 ;(function($, window, document, undefined){
 	var Myslideshow = function(ele, options){
+		var self = this;
+		this.$imgList = ele.find('#imgList');
+		this.defaults = {
 
+		};
+		this.options = $.extend({}, this.defaults , this.options);
 	};
 
 	Myslideshow.prototype = {
-
+		show: function(){
+			console.log(this.$imgList);
+		}
 	};
 	
 	$.fn.myslideshow = function(options){
 		var mySlideShow = new Myslideshow(this,options);
+			mySlideShow.show();
 	};
 })(jQuery, window, document);
