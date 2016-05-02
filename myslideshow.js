1483a4b716488d60a735c8dd59208d281653aea7
gyx13 <guoyuxuan13@gmail.com>
2016-05-03 00:51:58 +0800

    20160503

--- myslideshow.js
+++ myslideshow.js
@@ -1,21 +1,87 @@
+/*
+
+*/
+
+
+
 ;(function($, window, document, undefined){
 	var Myslideshow = function(ele, options){
 		var self = this;
-		this.$imgList = ele.find('#imgList');
-		this.defaults = {
-
+		self.defaults = {
+			autoplay: false,
+			delay: 3000,
+			speed: 750
 		};
-		this.options = $.extend({}, this.defaults , this.options);
+		self.settings = $.extend({}, this.defaults , this.options);
+
+		self.$slideshowWrapper = ele;
+		self.$imgList = ele.find('#imgList');
+		self.$prevArrow = ele.find('#prev');
+		self.$nextArrow = ele.find('#next');
+		self.$buttons = ele.find('#buttons');
+
+		self.$prevArrow.click(function(){
+			self.$imgList.eq(0).css('left', '-400px');
+		});
 	};
 
-	Myslideshow.prototype = {
-		show: function(){
-			console.log(this.$imgList);
-		}
+	function donotthing (){
+		console.log(this);
+	}
+
+	Myslideshow.prototype.show = function(){
+		console.log(this.$imgList);
 	};
+
+	Myslideshow.prototype.animate = function(){
+		console.log('300');
+	};
+
+	Myslideshow.prototype.initArrows = function(){
+
+		this.$prevArrow.click(function(){
+			// donotthing();
+		});
+
+		this.$nextArrow.click(function(){
+			
+		});
+	};
+
+	Myslideshow.prototype.init = function(){
+		this.initArrows();
+	};
+
 	
+	// Myslideshow.prototype = {
+	// 	show: function(){
+	// 		console.log(this.$imgList);
+	// 	},
+
+	// 	animate: function(){
+	// 		console.log('300');
+	// 	},
+
+	// 	initArrows: function(){
+	// 		this.$prevArrow.click(function(){
+	// 			console.log(this);
+	// 		});
+
+	// 		this.$nextArrow.click(function(){
+	// 			console.log('next');
+	// 		});
+	// 		return this;
+	// 	},
+
+	// 	init: function(){
+	// 		this.initArrows();
+	// 		return this;
+	// 	}
+	// };
+
+
 	$.fn.myslideshow = function(options){
 		var mySlideShow = new Myslideshow(this,options);
-			mySlideShow.show();
+			mySlideShow.init();
 	};
 })(jQuery, window, document);
